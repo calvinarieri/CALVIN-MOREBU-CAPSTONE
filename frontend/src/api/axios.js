c14e8f1417e75647cb6b85de87d41fcaf1a9a31a
¨calvin-arieri¨ <ariericalvin@gmail.com>
2026-08-03 13:45:41 +0300

    feat: fixed nginx

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const baseURL = import.meta.env.VITE_BASE_URL;
+export const baseURL = "/api";
 
 export const protectedAxiosInstance = axios.create({
     baseURL,
